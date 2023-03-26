9017e0f005057950cd30e56e70fa8656b20443f5
RovarQRoom <rovar.07000120@outlook.com>
2023-03-26 10:05:11 +0300

    Added Repository To Module

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -3,12 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Tasks, TasksSchema } from 'src/Model/TaskModel';
 import { TaskController } from './controller/task/task.controller';
 import { TaskService } from './service/task/task.service';
+import { TaskRepository } from './repository/task.repository';
 
 @Module({
     imports: [
         MongooseModule.forFeature([{name: Tasks.name, schema: TasksSchema}])
     ],
     controllers: [TaskController],
-    providers: [TaskService],
+    providers: [TaskService, TaskRepository],
 })
 export class TaskModule {}
