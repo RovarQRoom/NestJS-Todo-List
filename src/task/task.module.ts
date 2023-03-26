2a791a51b694ef37d13ba99cab8841f1c27c32b8
RovarQRoom <rovar.07000120@outlook.com>
2023-03-26 09:49:16 +0300

    Added Task Service

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Tasks, TasksSchema } from 'src/Model/TaskModel';
 import { TaskController } from './controller/task/task.controller';
+import { TaskService } from './service/task/task.service';
 
 @Module({
     imports: [
         MongooseModule.forFeature([{name: Tasks.name, schema: TasksSchema}])
     ],
     controllers: [TaskController],
+    providers: [TaskService],
 })
 export class TaskModule {}
