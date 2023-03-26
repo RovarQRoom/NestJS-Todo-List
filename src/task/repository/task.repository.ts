52a19e7c4454a14097048c5bae646b333f188ab0
RovarQRoom <rovar.07000120@outlook.com>
2023-03-26 10:04:22 +0300

    Added Interface To Task Repository And Service

--- src/task/repository/task.repository.ts
+++ src/task/repository/task.repository.ts
@@ -1,6 +1,22 @@
 import { Injectable } from "@nestjs/common";
+import { ITaskRepositoryInterface } from "../interface/Itaskrepository.interface";
 
 @Injectable()
-export class TaskRepository{
+export class TaskRepository implements ITaskRepositoryInterface{
+    createTask(createTaskDto: any): Promise<any> {
+        throw new Error("Method not implemented.");
+    }
+    getTasks(user: any): Promise<any> {
+        throw new Error("Method not implemented.");
+    }
+    getTaskById(id: string, user: any): Promise<any> {
+        throw new Error("Method not implemented.");
+    }
+    deleteTask(id: string, user: any): Promise<any> {
+        throw new Error("Method not implemented.");
+    }
+    updateTask(id: string, status: any, user: any): Promise<any> {
+        throw new Error("Method not implemented.");
+    }
 
 }
--- src/task/service/task/task.service.ts
+++ src/task/service/task/task.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { ITaskServiceInterface } from '../../interface/Itaskservice.interface';
 
 @Injectable()
-export class TaskService {}
+export class TaskService implements ITaskServiceInterface {
+    createTask(createTaskDto: any): Promise<any> {
+        throw new Error('Method not implemented.');
+    }
+    getTasks(user: any): Promise<any> {
+        throw new Error('Method not implemented.');
+    }
+    getTaskById(id: string, user: any): Promise<any> {
+        throw new Error('Method not implemented.');
+    }
+    deleteTask(id: string, user: any): Promise<any> {
+        throw new Error('Method not implemented.');
+    }
+    updateTask(id: string, user: any): Promise<any> {
+        throw new Error('Method not implemented.');
+    }
+    updateTaskStatus(id: string, status: any, user: any): Promise<any> {
+        throw new Error('Method not implemented.');
+    }
+}
