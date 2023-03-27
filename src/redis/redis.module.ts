c7afada42aa3e83197e3d5302685814ce7c6e0a4
RovarQRoom <rovar.07000120@outlook.com>
2023-03-27 10:27:10 +0300

    Commented Out The Microservice And Added Redis Cache Managment

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -1,37 +1,66 @@
-import { DynamicModule, FactoryProvider, Module, ModuleMetadata } from '@nestjs/common';
-import IORedis, {Redis, RedisOptions } from 'ioredis';
+import {CACHE_MANAGER ,CacheModule as BaseCacheModule, Inject, Logger, Module, OnModuleInit } from '@nestjs/common';
+import * as redisStore from 'cache-manager-ioredis';
+import { Cache } from 'cache-manager';
 
-export const IOREDISKEY = 'IOREDISKEY';
+// export const IOREDISKEY = 'IOREDISKEY';
 
-type RedisModuleOptions = {
-    connectionOptions: RedisOptions,
-    onClientReady?: (client: Redis) => void,
-};
+// type RedisModuleOptions = {
+//     connectionOptions: RedisOptions,
+//     onClientReady?: (client: Redis) => void,
+// };
 
-type RedisAsyncModuleOptions = {
-    useFactory: (...args: any[]) => Promise<RedisModuleOptions> | RedisModuleOptions,
-} & Pick<ModuleMetadata, 'imports'> & Pick<FactoryProvider, 'inject'>;
+// type RedisAsyncModuleOptions = {
+//     useFactory: (...args: any[]) => Promise<RedisModuleOptions> | RedisModuleOptions,
+// } & Pick<ModuleMetadata, 'imports'> & Pick<FactoryProvider, 'inject'>;
 
-@Module({})
-export class RedisModule {
-    static async registerAsync({useFactory, imports, inject}:RedisAsyncModuleOptions): Promise<DynamicModule> {
-        const redisProvider = {
-            provide: IOREDISKEY,
-            useFactory: async (...args: any[]) => {
-                const {connectionOptions, onClientReady} = await useFactory(...args);
-                const client = await new IORedis(connectionOptions);
-                if (onClientReady) {
-                    client.on('ready', () => onClientReady(client));
-                }
-                return client;
+// @Module({})
+// export class RedisModule {
+//     static async registerAsync({useFactory, imports, inject}:RedisAsyncModuleOptions): Promise<DynamicModule> {
+//         const redisProvider = {
+//             provide: IOREDISKEY,
+//             useFactory: async (...args: any[]) => {
+//                 const {connectionOptions, onClientReady} = await useFactory(...args);
+//                 const client = await new IORedis(connectionOptions);
+//                 if (onClientReady) {
+//                     client.on('ready', () => onClientReady(client));
+//                 }
+//                 return client;
+//             },
+//             inject,
+//         };
+//         return  {
+//             module:RedisModule,
+//             imports,
+//             providers: [redisProvider],
+//             exports: [redisProvider],
+//         };
+//     }
+// }
+
+//This Redis is For Microservice Communication
+
+
+//Using Redis As Cache
+@Module({
+    imports: [
+        BaseCacheModule.registerAsync({
+            useFactory: async () => {
+                return {
+                    store: redisStore,
+                    host: 'localhost',
+                    port: 6379,
+                    ttl: 60*3600*1000, // seconds
+                };
             },
-            inject,
-        };
-        return  {
-            module:RedisModule,
-            imports,
-            providers: [redisProvider],
-            exports: [redisProvider],
-        };
+        })
+    ],
+    providers: [],
+    exports: [BaseCacheModule],
+})
+export class RedisModule implements OnModuleInit {
+    constructor(@Inject(CACHE_MANAGER) private readonly cache: Cache) {}
+
+    public onModuleInit(): any {
+        const logger = new Logger('Cache');
     }
 }
