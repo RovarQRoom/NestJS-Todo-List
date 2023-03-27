4b84a5bdd23999835098a4c2211a7a3ea5e4bfe9
RovarQRoom <rovar.07000120@outlook.com>
2023-03-27 09:45:22 +0300

    Minor Fix

--- src/modules.config.ts
+++ src/modules.config.ts
@@ -1,3 +1,29 @@
 import {Logger} from "@nestjs/common";
 import {ConfigModule, ConfigService} from "@nestjs/config";
 import { RedisModule } from "./redis/redis.module";
+import { RedisOptions } from "ioredis";
+
+export const redisModule = RedisModule.registerAsync({
+    imports: [ConfigModule],
+    useFactory: async (configService: ConfigService) => {
+        const logger = new Logger('RedisModule');
+
+        const connectionOptions: RedisOptions = {
+            host: configService.get('REDIS_HOST') || 'localhost',
+            port: configService.get('REDIS_PORT') || 6379,
+        };
+        const onClientReady = (client) => {
+            logger.log('Redis client is ready');
+
+            client.on('error', (error) => {
+                logger.error("Redis Client Error: "+error);
+            });
+
+            client.on('connect', () => {
+                logger.log(`Connected to Redis: ${connectionOptions.host}:${connectionOptions.port}`);
+            });
+        };
+        return {connectionOptions, onClientReady};
+    },
+    inject: [ConfigService],
+});
